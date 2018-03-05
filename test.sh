9cf0bd4c8d41e794d56222c2dbbf3e7ab8e7582f
Raymond <musicsmithnz@gmail.com>
2018-03-06 02:44:53 +1300

    automated

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+user_name="user$(date +%s)"
+
+yum update -y
+yum install git -y
+
+repos=( "PINE_fogcdn.io" )
+github_url=( "https://github.com/musicsmithnz" )
+
+adduser ${user_name}
+cd /home/${user_name}
+su ${user_name}
+
 pkg_name="go-ipfs"
 pkg_v_no="v0.4.13"
 pkg_arch="linux-amd64"
@@ -8,5 +20,21 @@ pkg_ext="tar.gz"
 pkg_full_name="${pkg_name}_${pkg_v_no}_${pkg_arch}.${pkg_ext}"
 pkg_url="https://dist.ipfs.io/${pkg_name}/${pkg_v_no}/${pkg_full_name}"
 curl -O ${pkg_url}
+
 tar xvzf ${pkg_full_name} 
 
+exit
+pkg_name="go-ipfs"
+cd ${pkg_name}
+./install.sh
+cd ..
+
+su ${user_name}
+ipfs init
+ipfs daemon &
+
+for repo in ${repos[@]}; do
+    git clone "${github_url}/${repo}.git"
+    ipfs add -rw ${repo}
+done
+ipfs add ${pkg_full_name}
